1bffb039e93074cd749ca1112f63621f35109848
fagnersro <fagner.hg40@gmail.com>
2023-02-06 23:55:32 -0300

    installing redux, configuring redux environment

--- reduxapp/src/App.js
+++ reduxapp/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Rotas from './routes';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 export default function App() {
   return (
     <div>
-      <Rotas />
+      <Provider store={store}>
+        <Rotas />
+      </Provider>
     </div>
   );
 }
